f470e22b51f7618d93c11bdc4d5e470772656c22
Ngangman-Teddy <teddy.webdev@2005.com>
2024-04-15 11:18:39 +0100

    Initialize project using Create React App

---
+++ New folder/src/database/index.js
@@ -0,0 +1,38 @@
+ export const users = [
+    {
+        id: 1,
+        firstName: 'John',
+        lastName: 'Doe',
+        userName: 'JohnDoe237',
+        email: 'john@example.com',
+        password: 'john123'
+    },
+
+    {
+        id: 2,
+        firstName: 'Jane',
+        lastName: 'Cooper',
+        userName: 'JaneCooper237',
+        email: 'jane@example.com',
+        password: 'jane123'
+    },
+
+   { 
+        id: 3,
+        firstName: 'Tony',
+        lastName: 'Fax',
+        userName: 'TonyFax237',
+        email: 'tony@example.com',
+        password: 'tony123'
+    }, 
+    
+   { 
+        id: 4,
+        firstName: 'Mambo',
+        lastName: 'Blossom',
+        userName: 'Mambo Blossom',
+        email: 'mamboblossom@example.com',
+        password: '11'
+    }
+]
+
